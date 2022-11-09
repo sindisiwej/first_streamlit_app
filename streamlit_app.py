ded25b36ec64116b4c870cda67efbdec09b20a39
sindisiwej <63916501+sindisiwej@users.noreply.github.com>
2022-11-09 12:48:34 +0200

    Update streamlit_app.py
    add entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,9 @@ my_cur.execute("SELECT * FROM Fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The Fruit load List Contains:")
 streamlit.text(my_data_rows)
+
+
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
