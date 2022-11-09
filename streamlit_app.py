396a6072b729b64ff4a8a36679d13bff87e21c7a
sindisiwej <63916501+sindisiwej@users.noreply.github.com>
2022-11-09 13:09:29 +0200

    Update streamlit_app.py
    refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
    
    return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    if not fruit_choice:
@@ -41,15 +41,22 @@ try:
    else:
          back_from_function = get_fruityvice_data(fruit_choice)
          streamlit.dataframe(back_from_function)
-         
-
 except URLError as e:
   streamlit.error()
 
 
+streamlit.header("The fruit load list contains:!")
 
-
-# write your own comment -what does the next line do? 
+def get_fruit_load_list():
+   with mycnx.cursor() as my_cur:
+      my_cur.execute("SELECT * FROM Fruit_load_list")
+      return my_cur.fetchall()
+   
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = my_cur.get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+   
 
 # write your own comment - what does this do?
 
@@ -57,12 +64,8 @@ streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM Fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The Fruit load List Contains:")
-streamlit.text(my_data_rows)
+
+
 
 
 
